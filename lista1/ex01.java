5e8a7a6d6c4bc20d5174bb45e06ee3e4e4a7a205
SamaraParcero <samaraparceroti@gmail.com>
2023-06-04 13:14:38 -0300

    ex01
    ex01 lista 1

--- lista1/ex01.java
+++ lista1/ex01.java
@@ -1,10 +1,21 @@
 package lista1;
 
+import java.util.Scanner;
+
 public class ex01 {
 
     public static void main(String[] args){
+        int id, m, d, soma;
+		System.out.println("Digite sua idade em anos");
+		Scanner scan = new Scanner(System.in);
+		id = scan.nextInt();
+		System.out.println("Digite sua idade em meses");
+		m = scan.nextInt();
+		System.out.println("Digite sua idade em dias");
+		d = scan.nextInt();
+		soma = id*360 + m*30 + d;
+		System.out.println("Você viveu por incríveis " + soma + "dias");
 
-        
     }
 
 }
