7c048ed29203d259cdd6beffdf44dd52040e86d6
SamaraParcero <samaraparceroti@gmail.com>
2023-06-05 17:31:48 -0300

    ex03 lista3 condicional

---
+++ lista2/ex03.java
@@ -0,0 +1,21 @@
+package lista2;
+
+import java.util.Scanner;
+
+public class ex03 {
+    public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int Ano, Id;
+		System.out.println("Digite o ano de seu nascimento");
+		Ano = scan.nextInt();
+		Id= 2023-Ano;
+	    if (Id>= 16) {
+	    	System.out.println("Voce pode votar");
+	    } else {
+	    	System.out.println("Voce não pode votar");
+	    
+	    }
+        scan.close();
+	}
+  
+}
