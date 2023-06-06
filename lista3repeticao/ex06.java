8eedf8fbf805a6b3af4133a5b722401d216371dc
SamaraParcero <samaraparceroti@gmail.com>
2023-06-06 17:19:27 -0300

    ex06 lista3 repeticao

---
+++ lista3repeticao/ex06.java
@@ -0,0 +1,18 @@
+package lista3repeticao;
+
+import java.util.Scanner;
+
+public class ex06 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+         int a;
+         System.out.println("Digite um número");
+         a = scan.nextInt();
+    do {
+         System.out.println("Digite um número entre 0 e 100");
+         a=scan.nextInt(); 	
+    } while ((a<0)||(a>100));
+        System.out.println("Número correto");
+        scan.close();
+        }
+}
