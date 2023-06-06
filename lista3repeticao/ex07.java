bc357f8b40d635e00b0003d6306438ae581c38a8
SamaraParcero <samaraparceroti@gmail.com>
2023-06-06 17:21:13 -0300

    ex07 lista3 repeticao

---
+++ lista3repeticao/ex07.java
@@ -0,0 +1,19 @@
+package lista3repeticao;
+
+import java.util.Scanner;
+
+public class ex07 {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        int numero, divisores;
+        System.out.println("Digite um número");
+        numero= scan.nextInt();
+        for (divisores=1; numero%divisores==0; divisores++ ) {
+             System.out.println("Divisores:");
+             System.out.println(divisores);
+             
+        }
+        scan.close();
+     }
+
+}
