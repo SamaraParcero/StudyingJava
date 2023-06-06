27bbe36ed5d41b84e0cf2eb94fa4f25c75d8378e
SamaraParcero <samaraparceroti@gmail.com>
2023-06-06 17:26:29 -0300

    ex010 lista3 repetição

---
+++ lista3repeticao/ex010.java
@@ -0,0 +1,26 @@
+package lista3repeticao;
+
+import java.util.Scanner;
+
+public class ex010 {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        int N1, N2, menor, maior;
+        System.out.println("Digite um número");
+        N1= scan.nextInt();
+        System.out.println("Digite outro número");
+        N2= scan.nextInt();
+        if (N1<N2) {
+            
+            for (menor=N1 ;menor<=N2; menor++) {
+             System.out.println("Contagem: " + menor );
+         }
+        } else {
+            for (maior=N1 ;maior>=N2; maior-- ) {
+             System.out.println("Contagem: " + maior );
+        }
+     }
+     scan.close();
+     }
+
+}
