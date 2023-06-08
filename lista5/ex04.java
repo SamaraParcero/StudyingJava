89af0a4a00f816c184f201c314e53c556b140a48
SamaraParcero <samaraparceroti@gmail.com>
2023-06-08 20:51:40 -0300

    ex04 modularização lista5

--- lista5/ex04.java
+++ lista5/ex04.java
@@ -2,20 +2,22 @@ package lista5;
 
 import java.util.Scanner;
 
-public class ex03 {
+public class ex04 {
     public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String palavra;
 		System.out.println("Digite uma palavra qualquer: ");
 		palavra = scan.next();
 		digitacao(palavra);
-        scan.close();
+		scan.close();
 	}
 	public static void digitacao(String palavra) {
 		for (int c=0; c<palavra.length(); c++) {
-			System.out.println(palavra.charAt(c));
+		for (int l=0; l<=c; l++) {
+			System.out.print(palavra.charAt(l));
 		}
+		}
+		System.out.println();
 		
 	}
-
 }
