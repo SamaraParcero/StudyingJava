171328544d6a6a02ac80b3e2106a0a132f541a73
SamaraParcero <samaraparceroti@gmail.com>
2023-07-18 17:17:00 -0300

    começo estudo dio métodos

---
+++ lista6dio/Metodo.java
@@ -0,0 +1,14 @@
+package lista6dio;
+
+public class Metodo {
+    public static void main (String [] args){
+        String primeiroNome= "Samara";
+        String segundoNome = "Parcero";
+        String nomear = nomear(primeiroNome, segundoNome);
+        System.out.println(nomear);
+        }
+
+    public static String nomear (String primeiroNome, String segundoNome){
+        return "Seu nome é:" + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
