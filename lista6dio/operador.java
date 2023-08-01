0f932bf50a8e4ba400e8925ad02fe4ab5cab3857
SamaraParcero <samaraparceroti@gmail.com>
2023-08-01 18:16:47 -0300

    Update operador.java

--- lista6dio/operador.java
+++ lista6dio/operador.java
@@ -3,7 +3,7 @@ package lista6dio;
 public class operador {
     public static void main (String [] args){
      int numero = 5;
-     numero = numero -1;
+     numero--; //numero = numero -1;
      numero++; // numero = numero +1;
      numero = numero + numero;
      
