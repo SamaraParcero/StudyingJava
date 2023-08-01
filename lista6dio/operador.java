20105fbdbe779930e6edd8f82321b80aec0001e7
SamaraParcero <samaraparceroti@gmail.com>
2023-08-01 18:22:35 -0300

    Update operador.java

--- lista6dio/operador.java
+++ lista6dio/operador.java
@@ -6,9 +6,21 @@ public class operador {
      numero--; //numero = numero -1;
      numero++; // numero = numero +1;
      numero = numero + numero;
-     
+     int numero1 = 7;
         System.out.println(numero);
+       
 
+        boolean variavel = true;
+        System.out.println(!variavel); // "!" torna a variavel falsa
+
+     if (numero > numero1){
+        System.out.println(numero);
+     } else if (numero < numero1){
+        System.out.println(numero1);
+     } else {
+        System.out.println("iguais");
+     }
+      
     }
 
 }
