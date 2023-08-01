ebc00b0b95967a59de3b02ed2333e3ce5c9c77f4
SamaraParcero <samaraparceroti@gmail.com>
2023-08-01 18:13:46 -0300

    treinando operador

---
+++ lista6dio/operador.java
@@ -0,0 +1,14 @@
+package lista6dio;
+
+public class operador {
+    public static void main (String [] args){
+     int numero = 5;
+     numero = numero -1;
+     numero++; // numero = numero +1;
+     numero = numero + numero;
+     
+        System.out.println(numero);
+
+    }
+
+}
