4e50c6d43dca96d3e5f41a0e638c811fb1a77ff8
SamaraParcero <samaraparceroti@gmail.com>
2023-08-09 16:11:24 -0300

    for array

---
+++ lista6dio/ForArray.java
@@ -0,0 +1,9 @@
+public class ForArray {
+    public static void main(String[] args) {
+        String alunos [] = { "FELIPE", "JOANA", "JULIA", "MARCOS"};
+
+        for (int x=0; x < alunos.length; x++ ){
+            System.out.println( "O aluno " + x + " é " + alunos [x]);
+        }
+    }
+}
